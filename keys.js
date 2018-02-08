6e843a855287150eb5780ddc286fb83a3d7d992d
vrajdalal4741 <vrajdalal.popeyes@gmail.com>
2018-02-08 10:47:15 -0500

    1st push

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.yoOHZWM9OZzLSeq7TMe4ZjeEL,
+  consumer_secret: process.env.7su4SW1rjZmrLdb4XA6YhbKQFhR2u6qnqb80wrrihOtgwyAcH6,
+  access_token_key: process.env.959835191689842689-uihqB0ituQFzhiCgVjIuyJIoH1eysmB,
+  access_token_secret: process.env.AR13jFdD64YUPGBGaBOb2LxR6wFmJV1uK6pat1Gdh9LwF
+};
+
+exports.spotify = {
+  id: process.env.87f1b41b32804b47a87889ce1d00df25,
+  secret: process.env.c71762b5a6374c0f860f172ef2adb08f
+};
